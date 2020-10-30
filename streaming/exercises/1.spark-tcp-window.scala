74858c5686776498377278397b04a4e6df7992f2
mehdi-lamrani <mrk501@protonmail.com>
2020-10-30 12:12:52 +0100

    Update 1.spark-tcp-window.scala

--- streaming/exercises/1.spark-tcp-window.scala
+++ streaming/exercises/1.spark-tcp-window.scala
@@ -6,4 +6,7 @@ val dfc = df.groupBy(window($"timestamp", "10 second")).count()
 //OPTION 2 : AVG
 val dfc = df.groupBy(window($"timestamp", "10 second")).avg()
 
+//OPTION 3 : AVG
+val dfc = df.groupBy(window($"timestamp", "10 second", "1 second")).avg()
+
 dfc.writeStream.format("console").option("checkpointLocation", "/user/root/data/check").outputMode("Complete").start()
